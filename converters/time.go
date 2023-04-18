705cfdea20bcc537331c644e9eee54460731f605
youxiangyu <alanal@poweroak.com>
2023-04-18 14:51:44 +0800

    update: time converter

--- converters/time.go
+++ converters/time.go
@@ -7,6 +7,17 @@ import (
 	"github.com/jinzhu/copier"
 )
 
+func TimeInt64() []copier.TypeConverter {
+	return []copier.TypeConverter{
+		TimeToInt64(),
+		Int64ToTime(),
+		TimepToInt64p(),
+		Int64pToTimep(),
+		TimepToint64(),
+		Int64pToTime(),
+	}
+}
+
 func TimeToInt64() copier.TypeConverter {
 	return copier.TypeConverter{
 		SrcType: time.Time{},
@@ -33,6 +44,74 @@ func Int64ToTime() copier.TypeConverter {
 			}
 
 			return time.UnixMilli(s), nil
+		},
+	}
+}
+
+func Int64pToTimep() copier.TypeConverter {
+	var int64p *int64
+	var timep *time.Time
+	return copier.TypeConverter{
+		SrcType: int64p,
+		DstType: timep,
+		Fn: func(src interface{}) (interface{}, error) {
+			s, ok := src.(*int64)
+			if !ok {
+				return nil, errors.New("src type not matching")
+			}
+			t := time.UnixMilli(*s)
+			return &t, nil
+		},
+	}
+}
+
+func TimepToInt64p() copier.TypeConverter {
+	var int64p *int64
+	var timep *time.Time
+	return copier.TypeConverter{
+		SrcType: timep,
+		DstType: int64p,
+		Fn: func(src interface{}) (interface{}, error) {
+			s, ok := src.(*time.Time)
+			if !ok {
+				return nil, errors.New("src type not matching")
+			}
+			t := s.UnixMilli()
+			return &t, nil
+		},
+	}
+}
+
+func TimepToint64() copier.TypeConverter {
+	var int64p int64
+	var timep *time.Time
+	return copier.TypeConverter{
+		SrcType: timep,
+		DstType: int64p,
+		Fn: func(src interface{}) (interface{}, error) {
+			s, ok := src.(*time.Time)
+			if !ok {
+				return nil, errors.New("src type not matching")
+			}
+			t := s.UnixMilli()
+			return t, nil
+		},
+	}
+}
+
+func Int64pToTime() copier.TypeConverter {
+	var int64p *int64
+	var timep time.Time
+	return copier.TypeConverter{
+		SrcType: int64p,
+		DstType: timep,
+		Fn: func(src interface{}) (interface{}, error) {
+			s, ok := src.(*int64)
+			if !ok {
+				return nil, errors.New("src type not matching")
+			}
+			t := time.UnixMilli(*s)
+			return t, nil
 		},
 	}
 }
